f5ba915978b8759376e3c13a288c1aed1b7bd576
stiven vargas <stiven1693vargas@gmail.com>
2021-01-24 22:20:45 -0500

    ajuste implementación carro compras

--- src/java/ProductoController.java
+++ src/java/ProductoController.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+import Util.logCarritoCompras;
 import com.dto.Carrito;
 import com.dto.ProductoDto;
 import com.dto.ProductoDto1;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 /**
@@ -26,57 +27,115 @@ import javax.faces.context.FacesContext;
 @Named(value = "productoController")
 @SessionScoped
 public class ProductoController implements Serializable {
-    
-    ProductoDto listProduct= new ProductoDto();
-    ProductoFacadeImpl facadeImpl= new ProductoFacadeImpl();
-    ProductoDto1 productoDto1= new ProductoDto1();
-    List<Carrito> listaCarrito= new ArrayList<>();
+
+    ProductoDto listProduct = new ProductoDto();
+    ProductoFacadeImpl facadeImpl = new ProductoFacadeImpl();
+    ProductoDto1 productoDto1 = new ProductoDto1();
+    List<Carrito> listaCarrito = new ArrayList<>();
     int item;
-    double totalPagar=0.0;
-    int cantidad= 0;
-    int contador=0;
+    double totalPagar = 0.0;
+    int cantidad = 1;
+    int contador = 0;
+    logCarritoCompras log= new logCarritoCompras();
 
     /**
      * Creates a new instance of ProductoController
      */
     public ProductoController() {
-         facadeImpl= new ProductoFacadeImpl();
+
     }
-    
-    public String init(){
+
+    @PostConstruct
+    public void inicio() {
         try {
-            listProduct= facadeImpl.listarProductoService();
-         return "home";
+        facadeImpl = new ProductoFacadeImpl();
+        listProduct = facadeImpl.listarProductoService();
         } catch (Exception e) {
-            System.out.println("Error metodo int: " +e.getMessage());
+            log.setLogError("error en metodo de Inicio, consultando lista de productos" + e.getMessage());
+        }
+    }
+
+    public String init() {
+        try {
+            listProduct = facadeImpl.listarProductoService();
+            return "home";
+        } catch (Exception e) {
+            System.out.println("Error metodo int: " + e.getMessage());
+            log.setLogError("error al Iniciar" + e.getMessage());
             return "inicio";
         }
     }
-    
-    public void consultarProducto(int id){
+
+    public void consultarProducto(int id) {
         try {
-            productoDto1= facadeImpl.listarProductoId(id);
-            item= item+1;
-            Carrito carrito= new Carrito();
+            productoDto1 = facadeImpl.listarProductoId(id);
+            item = item + 1;
+            Carrito carrito = new Carrito();
             carrito.setItem(item);
             carrito.setIdProducto(productoDto1.getIdProducto());
             carrito.setNombre(productoDto1.getNombres());
             carrito.setDescripcion(productoDto1.getDescripcion());
             carrito.setPrecioCompra(productoDto1.getPrecio());
-            carrito.setCantidad(cantidad +1);
-            carrito.setSubTotal(cantidad*productoDto1.getPrecio());
+            carrito.setCantidad(cantidad);
+            carrito.setSubTotal(cantidad * productoDto1.getPrecio());
             listaCarrito.add(carrito);
-            contador= listaCarrito.size();
+            contador = listaCarrito.size();
         } catch (Exception e) {
-            
+            log.setLogError("error al consultar producto por id:" + e.getMessage());
         }
     }
-    
-    public void hacerCompra(){
-        
+
+    public String hacerCompra(int id) {
+        try {
+            totalPagar = 0.00;
+            productoDto1 = facadeImpl.listarProductoId(id);
+            item = item + 1;
+            Carrito carrito = new Carrito();
+            carrito.setItem(item);
+            carrito.setIdProducto(productoDto1.getIdProducto());
+            carrito.setNombre(productoDto1.getNombres());
+            carrito.setDescripcion(productoDto1.getDescripcion());
+            carrito.setPrecioCompra(productoDto1.getPrecio());
+            carrito.setCantidad(cantidad + 1);
+            carrito.setSubTotal(cantidad * productoDto1.getPrecio());
+            listaCarrito.add(carrito);
+            contador = listaCarrito.size();
+            for (int i = 0; i < listaCarrito.size(); i++) {
+                totalPagar = totalPagar + listaCarrito.get(i).getSubTotal();
+            }
+        } catch (Exception e) {
+            log.setLogError("error al consultar producto por id:" + e.getMessage());
+        }
+        return "Carrito";
     }
     
-    public String verCarrito(){
+    public void realizarPago(){
+        try {
+            if(!listaCarrito.isEmpty()){
+            FacesMessage facesMessage= new FacesMessage(FacesMessage.SEVERITY_INFO, "Pago Exitoso", null);
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+            }else{
+                FacesMessage facesMessage= new FacesMessage(FacesMessage.SEVERITY_ERROR, "Pago Exitoso", null);
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+            }
+        } catch (Exception e) {
+            log.setLogError("Error al realizar el pago:" + e.getMessage());
+        }
+    }
+
+    public void eliminarProducto(int id) {
+        for (int i = 0; i < listaCarrito.size(); i++) {
+            if(listaCarrito.get(i).getIdProducto() == id){
+                listaCarrito.remove(i);
+            }
+        }
+    }
+
+    public String verCarrito() {
+        totalPagar = 0.00;
+        for (int i = 0; i < listaCarrito.size(); i++) {
+            totalPagar = totalPagar + listaCarrito.get(i).getSubTotal();
+        }
         return "Carrito";
     }
 
@@ -135,8 +194,5 @@ public class ProductoController implements Serializable {
     public void setContador(int contador) {
         this.contador = contador;
     }
-    
-    
-    
-    
+
 }
